ec3f7b38aeaee344b03e12a4a99705175543633f
JMJarvis1 <97367692+JMJarvis1@users.noreply.github.com>
2024-04-15 21:44:09 -0400

    Srtring methods in progress

---
+++ Chapter_6/Chapter_6/put.py
@@ -0,0 +1,14 @@
+from curses.ascii import isalnum
+
+
+while True:
+    age = input("Enter your age:\n")
+    if age.isdecimal():
+        break
+    print("Please enter a number for your age.")
+
+while True:
+    password = input("Select a new password (letters and numbers only):\n")
+    if password.isalnum():
+        break
+    print("Passwords can only have letters and numbers.")
---
+++ Chapter_6/multiline.py
@@ -0,0 +1,14 @@
+"""This is a test Python program.
+Written by Al Sweigart al@iventwithpython.com
+
+This program qas desaigned for Python 3, not Python 2.
+"""
+
+
+def spam():
+    """TYhis is a multiline comment to help explain what
+    the spam() functions does."""
+    print("Hello!")
+
+
+spam()
