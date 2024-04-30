4d3a98336d5bd26f675c2bdfd8632bb3b7af597b
JMJarvis1 <97367692+JMJarvis1@users.noreply.github.com>
2024-04-29 20:51:49 -0400

    convertDates in progress

--- Chapter_7/convertDates.py
+++ Chapter_7/convertDates.py
@@ -6,12 +6,17 @@ import re
 
 # TODO build regex fo date recognition
 
-# TODO convert dates to 00/00/0000 format
-
 datePattern = re.compile(r"""
-    ([0]*[1-12]|\d{4}|)
+    ([a-zA-z]{3,9}(\.)?|\d{4})    # first segment   
+    ((-|\/|(\.)?\s)*              # separator
+    ([0-9]{1,2}[stnd.]*)          # Day or Month
+    ((-|\/|[\.,])(\s)?)           # separator
+    (([a-zA-z]{3,9}(\.)?)?        # month name (Apr./April)
+    ([0-3]{,1}[0-9])?             # month numeric
+    (\d{4}|)?)$                   # year numeric   
+""") 
 
-""")
+# TODO convert dates to 00/00/0000 format
 
 
 
