ae8d44fb7351f1f7dd857c00a6b28440bd989e09
JMJarvis1 <97367692+JMJarvis1@users.noreply.github.com>
2024-05-03 19:39:38 +0000

    Complete typoRegex dict

--- Chapter_7/typoChecker.py
+++ Chapter_7/typoChecker.py
@@ -6,7 +6,15 @@ import colorama
 
 
 def main():
-    pass
+    
+    # dict holding regex patterns for some common typos
+    typoRegex = {
+        "repeatWords": r"\b(\w+)\s+\1\b",                   # repeated repeated words
+        "repeatPunctuation": r"[/(?!%:;\"])\1|(\.{2})\2",   # punctuation,, repeated 
+        "extraSpace": r"\b( ){2,}",                         # too  many  spaces
+    }
 
+    text = ""      # Text to be examined
+    
 if __name__ == "__main__":
     main()
