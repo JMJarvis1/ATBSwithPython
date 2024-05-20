10d240f9411924a7d8b2cf2c64898382b8744711
JMJarvis1 <97367692+JMJarvis1@users.noreply.github.com>
2024-05-20 14:50:46 +0000

    Complete randomQuizGenerator Begin "multi-clipboard" mcb.pyw

--- Chapter_9/randomQuizGenerator.py
+++ Chapter_9/randomQuizGenerator.py
@@ -63,20 +63,43 @@ capitals = {
 
 for quizNum in range(35):
     # Create the quiz and answer key files.
-    quizFile = open(f"capitalsquiz{quizNum + 1}.txt", "w")
-    answerKeyFile = open(f"capitalsquiz_answers{quizNum + 1}.txt", "w")
+    quizFile = open(f"Chapter_9/CapQuiz/capitalsquiz{quizNum + 1}.txt", "w")
+    answerKeyFile = open(
+        f"Chapter_9/CapQuiz/capitalsquiz_answers{quizNum + 1}.txt", "w"
+    )
 
     # Write out the header for the quiz.
     quizFile.write("Name:\n\nDate:\n\nPeriod:\n\n")
-    quizFile.write((" " * 20) + f"State Capitals Quiz (From{quizNum + 1})")
+    quizFile.write((" " * 20) + f"State Capitals Quiz (Form {quizNum + 1})\n\n")
 
     # Shuffle the order of the states.
     states = list(capitals.keys())
     random.shuffle(states)
 
     # Loop through all 50 states, making a question for each
-    
-    for state in states:
-        
 
+    for questionNum in range(50):
+        # Get right an dwrong answers.
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
 
+        # Write the question and answer options to the quiz file.
+
+        quizFile.write(
+            f"{questionNum + 1}. What is the capital of {states[questionNum]}?\n"
+        )
+
+        for i in range(4):
+            quizFile.write(f"  {'ABCD'[i]}.{answerOptions[i]}\n")
+
+        # Write the answer key to a file.
+        answerKeyFile.write(
+            f"{questionNum + 1}. {'ABCD'[answerOptions.index(correctAnswer)]}\n"
+        )
+
+    quizFile.close()
+    answerKeyFile.close()
