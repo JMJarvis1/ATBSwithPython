fa8fe960fd34ee954749e83bd9ae66ba9ae3623d
JMJarvis1 <97367692+JMJarvis1@users.noreply.github.com>
2024-06-04 23:29:53 +0000

    Chapter 10: WIP (shutil, send2trash)

---
+++ Chapter_10/walk.py
@@ -0,0 +1,26 @@
+#! Python3
+# Exercise program -- os.walk()
+
+import os
+from pathlib import Path
+from stat import filemode
+
+p = Path.cwd() / "Chapter_10"
+os.chdir(p)
+
+for folderName, subfolders, fileNames in os.walk(p):
+   
+    message = f"The current folder is {folderName}\n"
+
+    for subfolder in subfolders:
+        message += f"  SUBFOLDER OF {os.path.basename(folderName)}: {subfolder}\n"
+        
+    for fileName in fileNames:
+       
+        message += f"  FILE INSIDE {os.path.basename(folderName)}: {fileName}\n"
+        message += f"\t The first line of text in {fileName} is:\n"
+
+    
+        
+
+    print(message + "\n")
